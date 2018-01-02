728ff9c29c29eea8d6f8cd7dfec8be9c8eac2b8b
Glitch (savory-bar) <none>
2018-01-02 22:24:05 +0000

    🚥🌅 Updated with Glitch

--- README.md
+++ README.md
@@ -1,36 +1,13 @@
-A template for a Twitter bot that responds to @ mentions and DMs
-================================================================
+# @AntipodeBot
 
-**Note:** You can see the bot [on Twitter](https://twitter.com/iamawakebot/with_replies).
+This is a remix of random-image-twitterbot. This twitter bot generates random coordinates that are exactly opposite on 🌎, also known as antipodes and then uses the Google Maps API to tweet the names and photos of those locations. You can check out the results [@AntipodeBot](https://www.twitter.com/AntipodeBot).
 
-Apps hosted on Glitch are automatically put to sleep after 5 minutes of inactivity (that is, if nobody opens your app in a browser window or it doesn't receive any data).
+## Created By:
+#### Courtney Brousseau
 
-Twitter's API doesn't send requests to your app, instead your app has to poll Twitter for data, which means, the bot will simply doze off after the first five minutes and will sleep through all the DMs and tweets it receives.
+[@cbrewsayso](https://www.twitter.com/cbrewsayso)
 
-If you want your bot to respond to @ mentions and DMs, you can wake it up every [25+ minutes](https://support.glitch.com/t/a-simple-twitter-bot-template/747/16) using a web service like [Uptime Robot](https://uptimerobot.com/), [cron-job.org](https://cron-job.org/en/), or [others](https://www.google.com/search?q=free+web+cron), have the bot search for tweets mentioning it, and respond to those tweets and any DMs it received.
+[courtneybrousseau.github.io](https://courtneybrousseau.github.io)
 
+[Github](https://github.com/CourtneyBrousseau/)
 
-![Tweetin'](https://cdn.gomix.com/4032b241-bff8-473e-aa6b-eb0c92a4bd06%2Ftweeting.gif)
-
-
-## A quick tutorial
-
-1. First, create a new Twitter account and a new Twitter app. ([This tutorial](https://botwiki.org/tutorials/how-to-create-a-twitter-app/) shows how.)
-2. Update the `.env` file with your Twitter API key/secrets. (The tutorial above explains how to get these.)
-3. Update `server.js` with some cool Twitter bot code.
-4. Set up a free service ([Uptime Robot](https://uptimerobot.com/), [cron-job.org](https://cron-job.org/en/), or [others](https://www.google.com/search?q=free+web+cron)) to wake up your bot every 25+ minutes and tweet. Use `https://YOURPROJECTNAME.glitch.me/tweet` as a URL to which to send the HTTP request.
-
-Check out [the Twit module documentation](https://github.com/ttezel/twit) for examples of what your bot can do, for example retweet a specific tweet by its ID:
-
-```
-T.post('statuses/retweet/:id', { id: '799687419259797504' }, function (err, data, response) {
-  console.log(data)
-});
-```
-You can find more [tutorials](https://botwiki.org/tutorials/twitterbots/#tutorials-nodejs) and [open source Twitter bots](https://botwiki.org/tag/twitter+bot+opensource+nodejs/) on [Botwiki](https://botwiki.org).
-
-And be sure to join the [Botmakers](https://botmakers.org/) online hangout and [submit your bot to Botwiki](https://botwiki.org/submit-your-bot) :-)
-
-**Powered by [Glitch](https://glitch.com)**
-
-\ ゜o゜)ノ
--- server.js
+++ server.js
@@ -1,36 +1,68 @@
-/* Setting things up. */
-var fs = require('fs'),
+var express = require('express'),
+    fs = require('fs'),
     path = require('path'),
-    req = require('request'),
-    express = require('express'),
+    request = require('request'),
     app = express(),   
     Twit = require('twit'),
-    https = require("https"),
-    randomFloat = require('random-float'),
-    gm = require('gm'),
     request = require('request'),
+    https = require('https'),
+    randomFloat = require('random-float'),
     config = {
-    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/make-an-image-posting-twitter-bot/#creating-a-twitter-app*/      
-      twitter: {
         consumer_key: process.env.CONSUMER_KEY,
         consumer_secret: process.env.CONSUMER_SECRET,
         access_token: process.env.ACCESS_TOKEN,
         access_token_secret: process.env.ACCESS_TOKEN_SECRET
-      }
-    },
-    T = new Twit(config.twitter);
+    };
 
-app.use(express.static('public'));
+var T = new Twit(config);
 
-/* You can use uptimerobot.com or a similar site to hit your /tweet endpoint to wake up your app and make your Twitter bot tweet. */
-  
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your bot is running on port ' + listener.address().port);
+// HELPER FUNCTIONS
+
+function random_from_array(images) {
+  return images[Math.floor(Math.random() * images.length)];
+}
+
+function getCountry(body) {
+  var address_info = body.results[0].address_components;
+  for (var i = 0; i < address_info.length; i ++) {
+    var info = address_info[i];
+    if (info.types.includes("country")) {
+      return info.long_name;
+    }
+  }
+}
+
+function getLocationName(body) {
+  var address_info = body.results[0].address_components;
+  var locationName = "";
+  for (var i = 0; i < address_info.length; i ++) {
+    var info = address_info[i];
+    if (info.types.includes("administrative_area_level_2")) {
+      locationName += info.long_name + ", ";
+    }
+    if (info.types.includes("administrative_area_level_1")) {
+      locationName += info.long_name + ", ";
+    }
+    if (info.types.includes("country")) {
+      locationName += info.long_name;
+    }
+  }
+  return locationName;
+}
+
+// WEBSITE
+
+app.get("/", function (request, response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write('<h1>random-image-twitterbot</h1><a href="https://glitch.com/edit/#!/random-image-twitterbot">See README.md</a>');  
+    response.end();  
 });
 
+// TWEET FUNCTION
+
 app.all("/tweet", function (request, response) {
+  console.log("Received a request...");
   generateLatsLons();
-  response.sendStatus(200);
 });
 
 function generateLatsLons() {
@@ -41,6 +73,10 @@ function generateLatsLons() {
   if (lon1 > 0) {
     lon2 = -lon2;
   }
+  
+  console.log(lat1 + ", " + lon1);
+  console.log(lat2 + ", " + lon2);
+  
   getLocation1(lat1, lon1, lat2, lon2);
 }
 
@@ -64,6 +100,7 @@ function getLocation1(lat1, lon1, lat2, lon2) {
       var body1 = JSON.parse(buffer);
       var status1 = body1.status;
       if (status1 == "OK" && getCountry(body1) != "Antarctica") {
+        console.log("Location 1: " + getLocationName(body1))
         getLocation2(body1, lat1, lon1, lat2, lon2);
       }
       else {
@@ -93,7 +130,8 @@ function getLocation2(body1, lat1, lon1, lat2, lon2) {
       var body2 = JSON.parse(buffer);
       var status2 = body2.status;
       if (status2 == "OK" && getCountry(body2) != "Antarctica") {
-        getImage1(body1, body2, lat1, lon1, lat2, lon2);
+        console.log("Location 2: " + getLocationName(body2))
+        uploadImage1(body1, body2, lat1, lon1, lat2, lon2);
       }
       else {
         generateLatsLons();
@@ -102,98 +140,61 @@ function getLocation2(body1, lat1, lon1, lat2, lon2) {
   });
 }
 
-function getImage1(body1, body2, lat1, lon1, lat2, lon2) {
-  // make URL
-  var src = "https://www.newyorker.com/wp-content/uploads/2014/05/140512_a18203-600.jpg";
-  // var src = "https://maps.googleapis.com/maps/api/staticmap?format=jpg&scale=2&maptype=satellite&center=" + lat1 + "," + lon1 + "&zoom=10&size=600x300&key=" + process.env.GOOGLE_API;
-  console.log(src);
-  var reqsrc = request(src);
-  console.log(reqsrc);
-  var gmreqsrc = gm(reqsrc);
-  console.log(gmreqsrc);
-  gmreqsrc.toBuffer('JPG', function(err, buffer) {
-    if (err) { 
-      console.log(err); 
-    } else {
-      var image1 = buffer.append('public/img/caption-bg.jpg').toString('base64');
-      T.post('media/upload', {media_data: image1}, function(err, data, response) {
-        if (err) { console.log(err); }
-        // Add meta data
-        var mediaIdStr1 = data.media_id_string;
-        var altText = lat1 + "," + lon1;
-        var meta_params = { media_id: mediaIdStr1, alt_text: { text: altText } }
-        // Post the tweet
-        T.post('media/metadata/create', meta_params, function (err, data, response) {
-          if (err) { console.log(err); }
-          if (!err) {
-            // now we can reference the media and post a tweet (media will attach to the tweet)
-            getImage2(body1, body2, lat1, lon1, lat2, lon2, mediaIdStr1);
-          }
-        })
+function uploadImage1(body1, body2, lat1, lon1, lat2, lon2) {
+  console.log('Image 1 loading ...');
+  var urls = ["https://maps.googleapis.com/maps/api/staticmap?format=jpg&scale=2&maptype=hybrid&center=" + lat1 + "," + lon1 + "&zoom=8&size=600x300&key=" + process.env.GOOGLE_API];
+  request({url: random_from_array(urls), encoding: null}, function (err, res, body) {
+    if (!err && res.statusCode == 200) {
+      var b64content = 'data:' + res.headers['content-type'] + ';base64,',
+          image = body.toString('base64');
+      console.log('Image 1 loaded!');
+      
+      T.post('media/upload', { media_data: image }, function (err, data, response) {
+        if (err){
+          console.log('ERROR:');
+          console.log(err);
+          // response.sendStatus(500);
+        }
+        else{
+          console.log('Image 1 uploaded!');
+          uploadImage2(body1, body2, lat1, lon1, lat2, lon2, data.media_id_string)
+        }
       });
     }
   });
 }
 
-function getImage2(body1, body2, lat1, lon1, lat2, lon2, mediaIdStr1) {
-  // make URL
-  var url = "https://maps.googleapis.com/maps/api/staticmap?format=jpg&scale=2&maptype=satellite&center=" + lat2 + "," + lon2 + "&zoom=10&size=600x300&key=" + process.env.GOOGLE_API;
-  console.log(url);
-  var request = https.get(url, function (response) {
-    // data is streamed in chunks from the server
-    // so we have to handle the "data" event    
-    var buffer = "", 
-          data,
-          route;
-
-      response.on("data", function (chunk) {
-          buffer += chunk;
-      }); 
-
-      response.on("end", function (err) {
-      // finished transferring data
-      // dump the raw data
-      constructTweet(body1, body2, lat1, lon1, lat2, lon2, mediaIdStr1);
-    });
+function uploadImage2(body1, body2, lat1, lon1, lat2, lon2, media_id_string1) {
+  console.log('Image 2 loading ...');
+  var urls = ["https://maps.googleapis.com/maps/api/staticmap?format=jpg&scale=2&maptype=hybrid&center=" + lat2 + "," + lon2 + "&zoom=8&size=600x300&key=" + process.env.GOOGLE_API];
+  request({url: random_from_array(urls), encoding: null}, function (err, res, body) {
+    if (!err && res.statusCode == 200) {
+      var b64content = 'data:' + res.headers['content-type'] + ';base64,',
+          image = body.toString('base64');
+      console.log('Image 2 loaded!');
+      
+      T.post('media/upload', { media_data: image }, function (err, data, response) {
+        if (err){
+          console.log('ERROR:');
+          console.log(err);
+          // response.sendStatus(500);
+        }
+        else{
+          console.log('Image 2 uploaded!');
+          postTweet(body1, body2, lat1, lon1, lat2, lon2, media_id_string1, data.media_id_string)
+        }
+      });
+    }
   });
 }
 
-
-
-function constructTweet(body1, body2, lat1, lon1, lat2, lon2, mediaIdStr1) {
-  tweet("📍 " + getLocationName(body1) + " | " + lat1 + ", " + lon1 + "\n\n\n" + "📍 " + getLocationName(body2) + " | " + lat2 + ", " + lon2, mediaIdStr1);
-}
-
-function getLocationName(body) {
-  var address_info = body.results[0].address_components;
-  var locationName = "";
-  for (var i = 0; i < address_info.length; i ++) {
-    var info = address_info[i];
-    if (info.types.includes("administrative_area_level_2")) {
-      locationName += info.long_name + ", ";
-    }
-    if (info.types.includes("administrative_area_level_1")) {
-      locationName += info.long_name + ", ";
-    }
-    if (info.types.includes("country")) {
-      locationName += info.long_name;
-    }
+function postTweet(body1, body2, lat1, lon1, lat2, lon2, media_id_string1, media_id_string2) {
+  var message = "📍 " + getLocationName(body1) + "\nLatitude: " + lat1 +  "\nLongitude: "+ lon1 + "\n" + "📍 " + getLocationName(body2) + "\nLatitude: " + lat2 +  "\nLongitude: "+ lon2;
+  var params = { 
+    status: message, 
+    media_ids: [media_id_string1, media_id_string2] 
   }
-  return locationName;
-}
-
-function getCountry(body) {
-  var address_info = body.results[0].address_components;
-  for (var i = 0; i < address_info.length; i ++) {
-    var info = address_info[i];
-    if (info.types.includes("country")) {
-      return info.long_name;
-    }
-  }
-}
-
-function tweet(message, mediaIdStr1) {
-  var params = { status: message, media_ids: [mediaIdStr1] }
+  
   T.post('statuses/update', params, function(err, data, response) {
     if (err) {
       /* TODO: Proper error handling? */
@@ -205,3 +206,7 @@ function tweet(message, mediaIdStr1) {
     }
   });
 }
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
